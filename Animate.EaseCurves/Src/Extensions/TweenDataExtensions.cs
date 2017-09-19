7815b5924aac271d7d8e05fb9751df0a28900cb2
felipegodias <felipe.g.o.dias@outlook.com>
2017-09-19 01:15:23 -0300

    Created extensions for the ITweenData.

---
+++ Animate.EaseCurves/Src/Extensions/TweenDataExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Animate.Core.Interfaces;
+
+namespace Animate.EaseCurves.Extensions {
+
+    /// <summary>
+    /// </summary>
+    public static class TweenDataExtensions {
+
+        /// <summary>
+        /// </summary>
+        /// <param name="tweenData"></param>
+        /// <param name="easeCurveName"></param>
+        /// <returns></returns>
+        public static ITweenData SetEaseCurve(this ITweenData tweenData, string easeCurveName) {
+            IEaseCurve easeCurve = EaseCurvesLibrary.GetEaseCurve(easeCurveName);
+            return tweenData.SetEaseCurve(easeCurve);
+        }
+
+    }
+
+}
