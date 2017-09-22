58f63b964ca876590f9d616b344ce6cb4f588b49
felipegodias <felipe.g.o.dias@outlook.com>
2017-09-22 00:57:37 -0300

    Created scale animations.

---
+++ Animate.Animations/Src/ScaleAnimations/ScaleToAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Animate.Animations.ScaleAnimations {
+
+    /// <summary>
+    /// </summary>
+    public class ScaleToAnimation : ScaleFromToAnimation {
+
+        /// <summary>
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="to"></param>
+        public ScaleToAnimation(Transform transform, Vector3 to) : base(transform, transform.localScale, to) {
+        }
+
+    }
+
+}
