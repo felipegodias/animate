5247732d69ea94679831dc2c129f30284945d62e
felipe.dias <felipe.dias@lumentech.cc>
2018-01-16 23:04:09 -0300

    Created base scriptable object editor.

--- Animate.Core.Editor/Src/ScriptableObjects/ScriptableObjectEditor.cs
+++ Animate.Core.Editor/Src/ScriptableObjects/ScriptableObjectEditor.cs
@@ -1,5 +1,131 @@
-﻿namespace Animate.Core.Editor.ScriptableObjects {
+﻿using Animate.Core.Editor.Styles;
+using UnityEditor;
+using UnityEngine;
 
-    public abstract class ScriptableObjectEditor : UnityEditor.Editor { }
+namespace Animate.Core.Editor.ScriptableObjects {
+
+    public abstract class ScriptableObjectEditor : UnityEditor.Editor {
+
+        /// <summary>
+        /// </summary>
+        private static ScriptableObjectStyle style;
+
+        /// <summary>
+        /// </summary>
+        private readonly float headerHeight = 45;
+
+        /// <summary>
+        /// </summary>
+        private readonly float headerOffset = 3;
+
+        /// <summary>
+        /// </summary>
+        private Rect iconRect = new Rect(9, 7, 25, 30);
+
+        /// <summary>
+        /// </summary>
+        private Rect titleRect = new Rect(-1, -3, 200, 19);
+
+        /// <summary>
+        /// </summary>
+        private Rect settingsRect = new Rect(-1, 6, 16, 16);
+
+        /// <summary>
+        /// </summary>
+        private Rect helpRect = new Rect(-3, 0, 16, 16);
+
+        /// <summary>
+        /// </summary>
+        private Rect headerArea;
+
+        /// <summary>
+        /// </summary>
+        private Rect iconArea;
+
+        /// <summary>
+        /// </summary>
+        private Rect titleArea;
+
+        /// <summary>
+        /// </summary>
+        private Rect settingsArea;
+
+        /// <summary>
+        /// </summary>
+        private Rect helpArea;
+
+        /// <summary>
+        /// </summary>
+        protected virtual bool DrawSettingsButton { get; }
+
+        /// <summary>
+        /// </summary>
+        protected virtual string HelpUrl { get; }
+
+        /// <summary>
+        /// </summary>
+        protected virtual string FallbackName { get; }
+
+        /// <summary>
+        /// </summary>
+        protected virtual void OnSettingsButtonClick() { }
+
+        /// <summary>
+        /// </summary>
+        protected virtual void OnMenuGUI() { }
+
+        /// <summary>
+        /// </summary>
+        protected virtual void OnContentGUI() { }
+
+        /// <summary>
+        /// </summary>
+        protected override void OnHeaderGUI() {
+            if (style == null) {
+                style = new ScriptableObjectStyle();
+            }
+
+            this.headerArea = EditorGUILayout.GetControlRect(false, this.headerHeight);
+
+            this.headerArea = new Rect(this.headerArea.xMin - this.headerOffset, this.headerArea.yMin,
+                this.headerArea.xMax + this.headerOffset,
+                this.headerArea.yMax);
+
+            this.iconArea = new Rect(this.headerArea.xMin + this.iconRect.xMin,
+                this.headerArea.yMin + this.iconRect.yMin, this.iconRect.width, this.iconRect.height);
+
+            this.titleArea = new Rect(this.iconArea.xMin + this.iconArea.xMax + this.titleRect.xMin,
+                this.iconArea.yMin + this.titleRect.yMin, this.titleRect.width, this.titleRect.height);
+
+            if (this.DrawSettingsButton) {
+                this.settingsArea = new Rect(this.headerArea.xMax + this.settingsRect.xMin - this.settingsRect.width,
+                    this.headerArea.yMin + this.settingsRect.yMin, this.settingsRect.width, this.settingsRect.height);
+
+                this.helpArea = new Rect(this.settingsArea.xMin + this.helpRect.xMin - this.helpRect.width,
+                    this.settingsArea.yMin + this.helpRect.yMin, this.helpRect.width, this.helpRect.height);
+            } else {
+                this.helpArea = new Rect(this.headerArea.xMax + this.settingsRect.xMin - this.helpArea.width,
+                    this.headerArea.yMin + this.settingsRect.yMin, this.helpArea.width, this.helpArea.height);
+            }
+
+            if (Event.current != null && Event.current.type == EventType.Repaint) {
+                int controlId = 0;
+                style.InspectorBig.Draw(this.headerArea, GUIContent.none, controlId++);
+                GUIStyle.none.Draw(this.iconArea, style.Icon, controlId++);
+                string title = this.target.name != string.Empty ? this.target.name : this.FallbackName;
+                title = ObjectNames.NicifyVariableName(title);
+                EditorStyles.largeLabel.Draw(this.titleArea, new GUIContent(title), controlId);
+            }
+
+            if (this.DrawSettingsButton && GUI.Button(this.settingsArea, style.SettingsIcon, GUIStyle.none)) {
+                this.OnSettingsButtonClick();
+            }
+
+            if (this.HelpUrl != null && GUI.Button(this.helpArea, style.HelpIcon, GUIStyle.none)) {
+                Application.OpenURL(this.HelpUrl);
+            }
+        }
+
+    }
 
 }
---
+++ Animate.Core.Editor/Src/Styles/ScriptableObjectStyle.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Animate.Core.Editor.Styles {
+
+    /// <summary>
+    /// </summary>
+    public class ScriptableObjectStyle {
+
+        /// <summary>
+        /// </summary>
+        private const string kEditorTypeSStylesField = "s_Styles";
+
+        /// <summary>
+        /// </summary>
+        private const string kSStylesTypeInspectorBigField = "inspectorBig";
+
+        /// <summary>
+        /// </summary>
+        private const string kEditorGUITypeGUIContentsField = "GUIContents";
+
+        /// <summary>
+        /// </summary>
+        private const string kGUIContentsTypeHelpIconField = "<helpIcon>k__BackingField";
+
+        /// <summary>
+        /// </summary>
+        private const string kGUIContentsTypeSettingsIconField = "<titleSettingsIcon>k__BackingField";
+
+        /// <summary>
+        /// </summary>
+        private const string kPluginIconNamespace = "Animate.Core.Editor.Resources.icon.png";
+
+        /// <summary>
+        /// </summary>
+        private GUIStyle inspectorBig;
+
+        /// <summary>
+        /// </summary>
+        private GUIContent helpIcon;
+
+        /// <summary>
+        /// </summary>
+        private GUIContent settingsIcon;
+
+        /// <summary>
+        /// </summary>
+        private GUIContent icon;
+
+        /// <summary>
+        /// </summary>
+        public GUIStyle InspectorBig {
+            get {
+                if (this.inspectorBig != null) {
+                    return this.inspectorBig;
+                }
+
+                Type editorType = typeof(UnityEditor.Editor);
+                FieldInfo sStylesFieldInfo = editorType.GetField(kEditorTypeSStylesField,
+                    BindingFlags.NonPublic | BindingFlags.Static);
+
+                if (sStylesFieldInfo == null) {
+                    this.inspectorBig = GUIStyle.none;
+                    return this.inspectorBig;
+                }
+
+                object sStyles = sStylesFieldInfo.GetValue(null);
+                if (sStyles == null) {
+                    sStyles = Activator.CreateInstance(sStylesFieldInfo.FieldType);
+                    sStylesFieldInfo.SetValue(null, sStyles);
+                }
+
+                Type styleType = sStyles.GetType();
+                FieldInfo inspectorBigFieldInfo = styleType.GetField(kSStylesTypeInspectorBigField);
+                this.inspectorBig = inspectorBigFieldInfo.GetValue(sStyles) as GUIStyle;
+                return this.inspectorBig;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        public GUIContent HelpIcon {
+            get {
+                if (this.helpIcon != null) {
+                    return this.helpIcon;
+                }
+
+                Type editorGuiType = typeof(EditorGUI);
+                Type guiContentsType =
+                    editorGuiType.GetNestedType(kEditorGUITypeGUIContentsField, BindingFlags.NonPublic);
+                FieldInfo helpIconFieldInfo = guiContentsType.GetField(kGUIContentsTypeHelpIconField,
+                    BindingFlags.Static | BindingFlags.NonPublic);
+
+                if (helpIconFieldInfo == null) {
+                    this.helpIcon = GUIContent.none;
+                    return this.helpIcon;
+                }
+
+                this.helpIcon = helpIconFieldInfo.GetValue(null) as GUIContent;
+                return this.helpIcon;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        public GUIContent SettingsIcon {
+            get {
+                if (this.settingsIcon != null) {
+                    return this.settingsIcon;
+                }
+
+                Type editorGuiType = typeof(EditorGUI);
+                Type guiContentsType =
+                    editorGuiType.GetNestedType(kEditorGUITypeGUIContentsField, BindingFlags.NonPublic);
+                FieldInfo titleSettingsIconFieldInfo = guiContentsType.GetField(
+                    kGUIContentsTypeSettingsIconField,
+                    BindingFlags.Static | BindingFlags.NonPublic);
+
+                if (titleSettingsIconFieldInfo == null) {
+                    this.settingsIcon = GUIContent.none;
+                    return this.settingsIcon;
+                }
+
+                this.settingsIcon = titleSettingsIconFieldInfo.GetValue(null) as GUIContent;
+                return this.settingsIcon;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        public GUIContent Icon {
+            get {
+                if (this.icon != null) {
+                    return this.icon;
+                }
+
+                Texture2D iconTexture = new Texture2D(4, 4);
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Stream file = assembly.GetManifestResourceStream(kPluginIconNamespace);
+
+                if (file == null) {
+                    this.icon = GUIContent.none;
+                    return this.icon;
+                }
+
+                byte[] imageData = new byte[file.Length];
+                file.Read(imageData, 0, (int) file.Length);
+                file.Dispose();
+
+                iconTexture.LoadImage(imageData);
+                this.icon = new GUIContent(iconTexture);
+                return this.icon;
+            }
+        }
+
+    }
+
+}
